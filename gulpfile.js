9aad0686547e6a99d2ee473425ee4232829cb8b2
rimrim990 <lim990718@naver.com>
2023-12-24 12:36:54 +0900

    chore: change gultfile format to esmodule

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,12 @@
-import gulp from 'gulp';
-const { task } = gulp;
+import gulp from "gulp";
+import babel from "gulp-babel";
 
-task('default', () => {
-  // 걸프 작업
-});
+function defaultTask() {
+  // node sources
+  gulp.src("es6/**/*.js").pipe(babel()).pipe(gulp.dest("dist"));
+
+  // browser sources
+  gulp.src("public/es6/**/*.js").pipe(babel()).pipe(gulp.dest("public/dist"));
+}
+
+export default defaultTask;
