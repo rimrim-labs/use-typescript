f0749ce5435583f9d6686bede63a07fe6d5c0557
rimrim990 <lim990718@naver.com>
2023-12-24 12:42:03 +0900

    chore: add eslint to gulp task

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,14 @@
 import gulp from "gulp";
 import babel from "gulp-babel";
+import eslint from "gulp-eslint";
 
 function defaultTask() {
+  // eslint
+  gulp
+    .src(["es6/**/*.js", "public/es6/**/*.js"])
+    .pipe(eslint())
+    .pipe(eslint.format());
+
   // node sources
   gulp.src("es6/**/*.js").pipe(babel()).pipe(gulp.dest("dist"));
 
