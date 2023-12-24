8dbd4ccaa194d7e8667779614fa29982faf8aa7a
rimrim990 <lim990718@naver.com>
2023-12-24 12:58:42 +0900

    chore: update file format

--- gulpfile.js
+++ gulpfile.js
@@ -2,18 +2,21 @@ import gulp from "gulp";
 import babel from "gulp-babel";
 import eslint from "gulp-eslint";
 
-function defaultTask() {
-  // eslint
-  gulp
+function eslintTask() {
+  return gulp
     .src(["es6/**/*.js", "public/es6/**/*.js"])
     .pipe(eslint())
     .pipe(eslint.format());
+}
 
-  // node sources
+function babelTask() {
   gulp.src("es6/**/*.js").pipe(babel()).pipe(gulp.dest("dist"));
 
   // browser sources
-  gulp.src("public/es6/**/*.js").pipe(babel()).pipe(gulp.dest("public/dist"));
+  return gulp
+    .src("public/es6/**/*.js")
+    .pipe(babel())
+    .pipe(gulp.dest("public/dist"));
 }
 
-export default defaultTask;
+export default gulp.series(eslintTask, babelTask);
