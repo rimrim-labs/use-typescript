954c5951d18a49245dfdc6504763abf8dedb6215
rimrim990 <lim990718@naver.com>
2023-12-27 13:54:07 +0900

    chore: apply eslint airbnb style guide

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,7 +3,8 @@ module.exports = {
     browser: true,
     es2020: true,
   },
-  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended',
+    'airbnb-base', 'airbnb-typescript/base'],
   overrides: [
     {
       env: {
@@ -15,6 +16,7 @@ module.exports = {
       },
     },
   ],
+  ignorePatterns: ['dist', 'node_modules'],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaVersion: 'latest',
