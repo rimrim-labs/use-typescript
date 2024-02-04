2ae5b75ea46c1322ab4648026f938ba5745600d4
rimrim990 <lim990718@naver.com>
2024-02-04 22:27:21 +0900

    refactor: refactor error check code

--- src/react/apis/configs/axiosConfig.ts
+++ src/react/apis/configs/axiosConfig.ts
@@ -9,11 +9,14 @@ export const instance = axios.create({
 
 // defining a custom error handler for all APIs
 const errorHandler = (error: AxiosError<ServeErrorResponse>) => {
-  const code = error.response?.data?.code;
   let serverError = new BaseError('문제가 생겼습니다. 다시 시도해주세요.');
 
-  if (code === 'InvalidRequest' || code === 'IllegalState') {
-    serverError = new BaseError(error.response?.data?.message!);
+  if (error.response) {
+    const { code, message } = error.response.data;
+
+    if (code === 'InvalidRequest' || code === 'IllegalState') {
+      serverError = new BaseError(message);
+    }
   }
 
   return Promise.reject(serverError);
