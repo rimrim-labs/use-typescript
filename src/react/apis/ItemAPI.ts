65d314db07469940d51668cef41973d3814b6520
rimrim990 <lim990718@naver.com>
2024-02-04 22:17:29 +0900

    refactor: move types dir into react

--- src/react/apis/ItemAPI.ts
+++ src/react/apis/ItemAPI.ts
@@ -1,6 +1,6 @@
 import { AxiosPromise } from 'axios';
 import { instance } from './configs/axiosConfig';
-import { FetchItemResponse } from '../../types/response/Item';
+import { FetchItemResponse } from '../types/response/Item';
 
 export const fetchItem = (id: number): AxiosPromise<FetchItemResponse> => {
   return instance.get(`/items/${id}`);
